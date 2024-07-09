99c20fc52d06b6381ccc6a2bc4c8804cdbb298e9
kutluhan <kutluhan321@gmail.com>
2024-07-09 03:02:34 +0300

    gitignore and some conf file changes

--- config/config.php
+++ config/config.php
@@ -10,11 +10,14 @@ define('URL', $url);
 
 const DEV_MODE = true;
 
+// Read database configuration from .ini file and store as array
+$config = parse_ini_file(BASEDIR . '/config/config.ini.php');
+
 // Database connection
-$host = 'bzoighyemc4n1alzqpup-mysql.services.clever-cloud.com';
-$dbname = 'bzoighyemc4n1alzqpup';
-$username = 'uzwnv0ianiryk6xp';
-$password = 'ZuIKwJqDz5gyR9C2uMvF';
+$host = $config['host'];
+$dbname = $config['dbname'];
+$username = $config['username'];
+$password = $config['password'];
 
 try {
     $db = new PDO("mysql:host=".$host.";dbname=".$dbname.";charset=utf8", $username, $password);
